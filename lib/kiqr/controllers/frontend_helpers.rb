4cb661e0c4d23a172db811a77cec78e05e4f7d6a
kjellberg <rk@youngmedia.se>
2021-08-23 12:53:20 +0200

    Rename kiqr_user_modules to kiqr_user_dropdown

--- lib/kiqr/controllers/frontend_helpers.rb
+++ lib/kiqr/controllers/frontend_helpers.rb
@@ -4,16 +4,13 @@ module Kiqr
       extend ActiveSupport::Concern
 
       included do
-        helper_method :kiqr_user_modules
+        helper_method :kiqr_user_dropdown
       end
 
-      def kiqr_user_modules
+      def kiqr_user_dropdown
         links = []
         Kiqr::USER_MODULES.select { |m| m[:display_at] == :user_dropdown }.each do |mod|
-          links << {
-            label: mod[:label],
-            path: mod[:route]
-          }
+          links << { label: mod[:label], path: mod[:route] }
         end
         links
       end
