d0606e7eca95c25ec2a780908e2542530bd96246
kjellberg <rk@youngmedia.se>
2021-08-23 22:02:15 +0200

    remove moddable

--- lib/kiqr.rb
+++ lib/kiqr.rb
@@ -13,22 +13,4 @@ module Kiqr
     autoload :Account, 'kiqr/models/account'
     autoload :Member, 'kiqr/models/member'
   end
-
-  # Constants which holds kiqr configuration for extensions. Those should
-  # not be modified by the "end user" (this is why they are constants).
-  ALL_MODULES = []
-  USER_MODULES = []
-
-  def self.add_module(module_name, options = {})
-    insert_at = (options[:insert_at] || -1)
-    ALL_MODULES.insert insert_at, module_name
-
-    raise "Missing route parameter for module #{module_name}" unless (route = options[:route])
-
-    USER_MODULES.insert insert_at, {
-      label: (options[:label] || module_name.capitalize),
-      route: route,
-      display_at: (options[:display_at] || nil)
-    }
-  end
 end
--- lib/kiqr/engine.rb
+++ lib/kiqr/engine.rb
@@ -12,9 +12,6 @@ module Kiqr
       # Make Kiqr controller helpers available in all app controllers.
       ActionController::Base.include(Kiqr::Controllers::SetCurrentRequestDetails)
       ActionController::Base.include(Kiqr::Controllers::CurrentHelpers)
-      ActionController::Base.include(Kiqr::Controllers::FrontendHelpers)
-
-      Kiqr.add_module 'accounts', label: 'Switch account', route: :accounts, display_at: :user_dropdown
     end
   end
 end
