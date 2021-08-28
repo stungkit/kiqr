436e86befbf752eb87ae8e042915ced681c7411e
kjellberg <rk@youngmedia.se>
2021-08-28 20:30:44 +0200

    set_flash_message

--- app/controllers/kiqr_controller.rb
+++ app/controllers/kiqr_controller.rb
@@ -17,6 +17,34 @@ class KiqrController < Kiqr.config.parent_controller.constantize
     @current_ability ||= Ability.new(current_member)
   end
 
+  # Sets the flash message with :key, using I18n. By default you are able
+  # to set up your messages using specific resource scope, and if no message is
+  # found we look to the default scope. Set the "now" options key to a true
+  # value to populate the flash.now hash in lieu of the default flash hash (so
+  # the flash message will be available to the current action instead of the
+  # next action).
+  def set_flash_message(key, kind, options = {})
+    message = find_message(kind, options)
+    return if message.blank?
+
+    if options[:now]
+      flash.now[key] = message
+    else
+      flash[key] = message
+    end
+  end
+
+  # Get message for given
+  def find_message(kind, options = {})
+    options[:scope] ||= translation_scope
+    I18n.t(kind, **options)
+  end
+
+  # Default translation scope for messages
+  def translation_scope
+    "kiqr.#{controller_name}"
+  end
+
   private
 
   def set_account
