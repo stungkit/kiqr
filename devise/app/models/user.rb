fc7449df85be19e2e4a98898802562baa8c123f1
kjellberg <rk@youngmedia.se>
2021-08-31 23:04:45 +0200

    kiqr_devise

--- devise/app/models/user.rb
+++ devise/app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   include Kiqr::Models::User
   devise :database_authenticatable, :registerable,
--- devise/spec/models/user_spec.rb
+++ devise/spec/models/user_spec.rb
@@ -5,9 +5,6 @@ require 'spec_helper'
 module Kiqr
   RSpec.describe User, type: :model do
     pending 'should be a Devise model'
-    pending 'should not be able to destroy if owner of a team with an active subscription'
-
-    it { should belong_to(:account) }
-    it { should have_many(:accounts).through(:members) }
+    pending 'should successfully include Kiqr::Models::User'
   end
 end
