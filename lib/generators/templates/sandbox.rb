2d830b0d22ed3e4ef10055caaf9e839d6a7d3151
kjellberg <rk@youngmedia.se>
2021-09-04 13:56:32 +0200

    kiqr cli installer

--- lib/generators/templates/sandbox.rb
+++ lib/generators/templates/sandbox.rb
@@ -1,6 +1,75 @@
 # frozen_string_literal: true
 
-gem 'kiqr', path: '..'
-gem 'kiqr_devise', path: '..'
+KIQR_VERSION = '1.0.0.alpha'
+# frozen_string_literal: true
 
-rails_command 'generate kiqr:install user'
+# GEMFILE
+########################################
+inject_into_file 'Gemfile', before: 'group :development, :test do' do
+  <<~RUBY
+  # -------- Kiqr dependencies ---------
+
+  # Required:
+  gem 'devise', '~> 4.8.0'
+  gem 'kiqr_core', path: '..'
+
+  # Addons (optional):
+  gem 'turbo-rails', '0.7.11'
+  gem 'stimulus-rails', '0.3.11'
+  gem 'responders', git: 'https://github.com/heartcombo/responders' # until Responders gem is published with version >= 3.0.2.
+
+  gem 'tailwindcss-rails'
+  gem 'heroicon'
+  # ----- End of Kiqr dependencies -----
+
+  RUBY
+end
+
+# Uncomment to uncomment Redis in Gemfile
+# gsub_file('Gemfile', /# gem 'redis'/, "gem 'redis'")
+
+# Environment configs
+########################################
+env_config = <<~RUBY
+  config.action_mailer.default_url_options = {host: "http://localhost:3000"}
+RUBY
+
+environment env_config, env: 'development'
+
+after_bundle do
+  generate(:controller, 'pages', 'home', '--skip-routes', '--no-test-framework')
+
+  # Routes
+  ########################################
+  route "root to: 'pages#home'"
+
+  # Git ignore
+  ########################################
+  append_file '.gitignore', <<~TXT
+    # Ignore .env file containing credentials.
+    .env*
+    # Ignore Mac and Linux file system files
+    *.swp
+    .DS_Store
+  TXT
+
+  # Install hotwire
+  rails_command('turbo:install')
+  rails_command('stimulus:install')
+
+  # TailwindCSS install
+  rails_command('tailwindcss:install')
+
+  # Devise install + user
+  ########################################
+  generate('devise:install')
+  generate('devise', 'User')
+
+  # Kiqr install
+  generate('kiqr:install', 'user')
+end
+
+run 'touch .env'
+
+git add: '.'
+git commit: "-m 'Kiqr sandbox'"
--- tasks/update_versions.rb
+++ tasks/update_versions.rb
@@ -5,6 +5,13 @@ version = File.read("#{root}/KIQR_VERSION").strip
 
 desc 'Update version.rb files with KIQR_VERSION in all projects'
 task :update_versions do
+
+  file = 'lib/generators/templates/default.rb'
+  ruby = File.read(file)
+  ruby.gsub!(/^(\s*)KIQR_VERSION(\s*)= .*?$/, "\\1KIQR_VERSION = '#{version}'")
+  raise "Could not insert version in #{file}" unless Regexp.last_match(1)
+  File.open(file, 'w') { |f| f.write ruby }
+
   KIQR_GEMS.each do |gem_name|
     glob = root.dup
     glob << "/#{gem_name}/lib/**"
