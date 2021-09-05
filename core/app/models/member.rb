9c62d5d823c99400e0584accca3433e8f1f423dd
kjellberg <rk@youngmedia.se>
2021-09-05 23:53:57 +0200

    FIRST_USER_ROLE as string

--- core/app/models/member.rb
+++ core/app/models/member.rb
@@ -4,5 +4,5 @@ class Member < ApplicationRecord
   include Kiqr::Models::Member
 
   AVAILABLE_ROLES = %i[member admin].freeze
-  FIRST_USER_ROLE = :admin # Default role for the user who created the account.
+  FIRST_USER_ROLE = 'admin' # Default role for the user who created the account.
 end
