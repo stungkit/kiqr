405f1b64d6e98d90be996e054d07bf10a89b1026
kjellberg <rk@youngmedia.se>
2021-09-07 00:19:24 +0200

    Test email validations

--- core/spec/models/account_spec.rb
+++ core/spec/models/account_spec.rb
@@ -12,6 +12,8 @@ module Kiqr
     describe 'validations' do
       it { should validate_presence_of(:name) }
       it { should validate_presence_of(:billing_email) }
+      it { should allow_values('valid@mail.com').for(:billing_email) }
+      it { should_not allow_values('invalid@9xijn9ijxi9okiaf.com').for(:billing_email) }
     end
   end
 end
